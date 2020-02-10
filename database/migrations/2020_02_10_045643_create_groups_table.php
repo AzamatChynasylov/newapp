a4f981326f9903907e47a086b0e55e08f353009b
Azamat Chynasylov <azakgcool@gmail.com>
2020-02-10 11:13:07 +0600

    Create Group

---
+++ database/migrations/2020_02_10_045643_create_groups_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+						$table->bigIncrements('id');
+						$table->string('title');
+						$table->integer('parent_id')->nullable();
+						$table->tinyInteger('published')->nullable();
+						$table->tinyInteger('level')->nullable();
+						$table->integer('created_by')->nullable();
+						$table->integer('modified_by')->nullable();
+						$table->date('last_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
