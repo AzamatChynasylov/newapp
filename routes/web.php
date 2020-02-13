387b006d61f223218ab0ca93fa743d01b6fbab64
Azamat Chynasylov <azakgcool@gmail.com>
2020-02-13 14:49:53 +0600

    Create Questionnare MC

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', function () {
 Route::group(['prefix'=>'admin', 'namespace'=>'Admin', 'middleware'=>['auth']], function(){
 	Route::get('/','DashboardController@index')->name('admin.index');
 	Route::resource('/group', 'GroupController',['as'=>'admin']);
+	Route::resource('/questionnares', 'QuestionnareController');
 });
 
 
