ee47a9965edf48ff5eb42c1084357239b807eefc
Azamat Chynasylov <azakgcool@gmail.com>
2020-02-17 23:12:31 +0600

    realtime App

--- public/js/app.js
+++ public/js/app.js
@@ -84237,8 +84237,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\Desktop\newapp\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\Desktop\newapp\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\azamat\Desktop\newapp\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\azamat\Desktop\newapp\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
